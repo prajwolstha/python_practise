b320b38f2c678bd923a1d75f6dd146a245eb61d3
prajwolstha <prazolstha12345@gmail.com>
2025-08-26 11:05:12 +0545

    Initial commit

---
+++ module.py
@@ -0,0 +1,14 @@
+import pyjokes
+#print ("printing jokes")
+
+"""
+afdajfdsa;lfkdsa;lfdsafsaf
+multi line comment
+"""
+
+# hello this is the comment 
+# and 
+# there is a license
+
+joke = pyjokes.get_joke()
+print(joke)
