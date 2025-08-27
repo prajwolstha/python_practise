dde52f8f4533dda7b95723e70ee701b92daf211b
prajwolstha <prazolstha12345@gmail.com>
2025-08-27 20:10:06 +0545

    chapter 3 added

---
+++ chapter 3/slicing.py
@@ -0,0 +1,7 @@
+name = "prajwol"
+print(name[0:3])
+print(name[-4: -1])
+print(name[3:6])
+print(name[2:])
+a = "abcdefghijklmnopqrstuvwxyz"
+print(a[1:15:2])#skip the value by 2 (bwtwaeen 1 to 15 character)
---
+++ chapter 3/string_intro.py
@@ -0,0 +1,5 @@
+name ="prajwol"
+nameshort = name[0:5]
+print(nameshort)
+char1 = name[1]
+print(char1)
